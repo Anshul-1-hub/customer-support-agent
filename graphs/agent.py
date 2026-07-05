a9baab02a5a01c1ad91bf46d2e70f20bcb2c69e3
Anshul Rai <anshulrai1035@gmail.com>
2026-07-05 20:01:22 +0000

    add clarify, escalate and retrieve_and_resolve nodes

--- graphs/agent.py
+++ graphs/agent.py
@@ -3,6 +3,7 @@ from langchain_core.documents import Document
 from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
+from chains.retriever import get_retriever
 
 load_dotenv()
 
@@ -30,5 +31,37 @@ def check_clarity(state: AgentState) -> AgentState:
 
     else:
         state["is_clear"] = False
+
+    return state
+
+def clarify(state: AgentState) -> AgentState:
+    prompt=f"""Please ask the user to clarify the message they are trying to ask in this ticket.
+    Ticket:{state["ticket"]}
+    Reply with a reply asking the user , return a string only with the reply contained in it.
+    """
+
+    result = llm.invoke(prompt)
+    state["response"] = result.content
+
+    return state
+
+def escalate(state: AgentState) -> AgentState:
+    state["response"] = "Your issue ticket is being handed over to our support staff."
+    return state
+
+def retrieve_and_resolve(state: AgentState) -> AgentState:
+    retriever = get_retriever()
+    state["chunks"] = retriever.invoke(state["ticket"])
+    prompt=f"""Resolve this ticket's issue using the given chunks of data
+    Ticket:{state["ticket"]}
+    Chunks:{state["chunks"]}
+    Reply only if you find the answer or have knowledge about it, or else just say "NO"
+    """
+    result = llm.invoke(prompt)
+    if(result.content.strip().upper() == "NO"):
+        state["requires_escalation"] = True
+    else:
+        state["response"] = result.content
+        state["requires_escalation"] = False
 
     return state
